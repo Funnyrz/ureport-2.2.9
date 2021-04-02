22e4dc25c111286ccb5cbbaaabf667f8b0f156da
Funny <746108479@qq.com>
2021-04-02 10:55:21 +0800

    提交代码

--- ureport2-js/.history/src/tree/RemoteData_20210105153723.js
+++
@@ -1,4 +0,0 @@
-import uuid from 'node-uuid';
-import {alert,confirm,dialog} from '../MsgBox.js';
-import SqlDatasetDialog from '../dialog/SqlDatasetDialog.js';
-import BaseTree from './BaseTree.js';
